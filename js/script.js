71ff536f9c4de4114672dded1f23ee59157b7f32
academiaantioquia <140843034+academiaantioquia@users.noreply.github.com>
2023-11-08 18:26:05 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,3 +35,36 @@ $(document).ready(function(){
 		} setTimeout(showAll,400);
 	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+	//responsive menu toggle
+	$("#menutoggle").click(function() {
+	  $('.xs-menu').toggleClass('displaynone');
+
+	  });
+	//add active class on menu
+	$('ul li').click(function(e) {
+	  e.preventDefault();
+	  $('li').removeClass('active');
+	  $(this).addClass('active');
+	});
+  //drop down menu  
+	  $(".drop-down").hover(function() {
+		$('.mega-menu').addClass('display-on');
+	  });
+	  $(".drop-down").mouseleave(function() {
+		$('.mega-menu').removeClass('display-on');
+	  });
+  
+  });
