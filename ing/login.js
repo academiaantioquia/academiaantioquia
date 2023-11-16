a163cfc9394e35cb2ffb2a55b9a4821d216b506e
academiaantioquia <140843034+academiaantioquia@users.noreply.github.com>
2023-11-16 14:49:24 -0500

    Add files via upload

---
+++ ing/login.js
@@ -0,0 +1,18 @@
+function login(){
+    var user, psss;
+
+user = document.getElementById("user").value;
+psss = document.getElementById("psss").value;
+
+
+if(user == "miguel" && psss == "1234"){
+
+
+    window.location = "/admin/admin.html";
+
+
+}
+
+
+}
+
